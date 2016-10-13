5cda5f8853fcbeeeae546be65490718ab405fa8b
jbauza <jbauza@antel.net.uy>
2016-10-13 09:13:23 -0300

    Se agrega método CancelarTicket

--- src/webserviceIMM/WebserviceTicketsIMM.java
+++ src/webserviceIMM/WebserviceTicketsIMM.java
@@ -6,9 +6,19 @@ import capa_Negocio.ManagerTransacciones;
 
 @WebService
 public class WebserviceTicketsIMM {
+	
 	public DatatypeVenta Venta (DatatypeVenta dataTypeVenta){
 		ManagerTransacciones managerTransacciones = ManagerTransacciones.getInstance();
+		
 		return managerTransacciones.Venta(dataTypeVenta);
+	}
+	
+	public void CancelarTicket(long NroTicket, long idAgencia, String secretoAgencia){
+		
+		ManagerTransacciones managerTransacciones = ManagerTransacciones.getInstance();
+		
+		managerTransacciones.cancelarTicket(NroTicket, idAgencia, secretoAgencia);
+		
 	}
 
 }
